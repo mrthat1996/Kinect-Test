b1e423210dfcc0a95b7c1d6f9c2e1763b19c6253
mrthat1996 <mrthat1996>
2019-11-05 11:05:42 +0700

    init project

---
+++ KinectTest/frmTest.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace KinectTest
+{
+    partial class frmTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbPreview = new System.Windows.Forms.PictureBox();
+            this.cbType = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbPreview
+            // 
+            this.pbPreview.Location = new System.Drawing.Point(12, 37);
+            this.pbPreview.Name = "pbPreview";
+            this.pbPreview.Size = new System.Drawing.Size(460, 303);
+            this.pbPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPreview.TabIndex = 0;
+            this.pbPreview.TabStop = false;
+            // 
+            // cbType
+            // 
+            this.cbType.FormattingEnabled = true;
+            this.cbType.Items.AddRange(new object[] {
+            "color",
+            "depth"});
+            this.cbType.Location = new System.Drawing.Point(49, 10);
+            this.cbType.Name = "cbType";
+            this.cbType.Size = new System.Drawing.Size(423, 21);
+            this.cbType.TabIndex = 1;
+            this.cbType.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Type";
+            // 
+            // frmTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 349);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbType);
+            this.Controls.Add(this.pbPreview);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmTest";
+            this.Text = "Kinect Test";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmTest_FormClosing);
+            this.Load += new System.EventHandler(this.frmTest_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbPreview;
+        private System.Windows.Forms.ComboBox cbType;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ KinectTest/frmTest.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KinectTest
+{
+    public partial class frmTest : Form
+    {
+        private KinectSensor kinectSensor = null;
+        private MultiSourceFrameReader multiSourceFrameReader = null;
+
+        public frmTest()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTest_Load(object sender, EventArgs e)
+        {
+            kinectSensor = KinectSensor.GetDefault();
+            if (kinectSensor != null)
+            {
+                kinectSensor.Open();
+            }
+            cbType.Text = "color";
+        }
+
+        private void MultiSourceFrameReader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
+        {
+            if (cbType.Text == "color")
+            {
+                processColorFrame(e);
+            }
+            else if (cbType.Text == "depth")
+            {
+                processDepthFrame(e);
+            }
+        }
+
+        private void processColorFrame(MultiSourceFrameArrivedEventArgs e)
+        {
+            var refFrame = e.FrameReference.AcquireFrame();
+            using (var frame = refFrame.ColorFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    var width = frame.FrameDescription.Width;
+                    var height = frame.FrameDescription.Height;
+                    var data = new byte[width * height * 4];
+                    frame.CopyConvertedFrameDataToArray(data, ColorImageFormat.Bgra);
+                    var bitmap = new Bitmap(width, height, PixelFormat.Format32bppRgb);
+                    var bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.WriteOnly, bitmap.PixelFormat);
+                    Marshal.Copy(data, 0, bitmapData.Scan0, data.Length);
+                    bitmap.UnlockBits(bitmapData);
+                    pbPreview.Image = bitmap;
+                }
+            }
+        }
+
+        private void processDepthFrame(MultiSourceFrameArrivedEventArgs e)
+        {
+            var refFrame = e.FrameReference.AcquireFrame();
+            using (DepthFrame frame = refFrame.DepthFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    var width = frame.FrameDescription.Width;
+                    var height = frame.FrameDescription.Height;
+                    var data = new ushort[width * height];
+                    frame.CopyFrameDataToArray(data);
+
+                    int minDepth = frame.DepthMinReliableDistance;
+                    int maxDepth = frame.DepthMaxReliableDistance;
+
+                    int colorPixelIndex = 0;
+                    var newData = new byte[width * height * 4];
+                    for (int i = 0; i < data.Length; ++i)
+                    {
+                        ushort depth = data[i];
+                        byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
+
+                        // Blue
+                        newData[colorPixelIndex++] = intensity;
+
+                        // Green
+                        newData[colorPixelIndex++] = intensity;
+
+                        // Red
+                        newData[colorPixelIndex++] = intensity;
+
+                        // Alpha
+                        newData[colorPixelIndex++] = 100;
+                    }
+                    var bitmap = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+                    var bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.WriteOnly, bitmap.PixelFormat);
+                    Marshal.Copy(newData, 0, bitmapData.Scan0, newData.Length);
+                    bitmap.UnlockBits(bitmapData);
+                    pbPreview.Image = bitmap;
+                }
+            }
+        }
+
+        private void frmTest_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (kinectSensor != null)
+            {
+                kinectSensor.Close();
+                kinectSensor = null;
+            }
+
+            if (multiSourceFrameReader != null)
+            {
+                multiSourceFrameReader.Dispose();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbType.Text == "color")
+            {
+                multiSourceFrameReader = kinectSensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color);
+            }
+            else if (cbType.Text == "depth")
+            {
+                multiSourceFrameReader = kinectSensor.OpenMultiSourceFrameReader(FrameSourceTypes.Depth);
+            }
+            multiSourceFrameReader.MultiSourceFrameArrived -= MultiSourceFrameReader_MultiSourceFrameArrived;
+            multiSourceFrameReader.MultiSourceFrameArrived += MultiSourceFrameReader_MultiSourceFrameArrived;
+        }
+    }
+}
